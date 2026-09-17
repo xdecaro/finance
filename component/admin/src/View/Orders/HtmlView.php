0290aa1eafba4997a4778120cbcb9b26cc8523cf
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-18 01:12:21 +0200

    Finance 1.4.0: add accounting administrator views

---
+++ component/admin/src/View/Orders/HtmlView.php
@@ -0,0 +1,33 @@
+<?php
+namespace Xdecaro\Component\Decarofinance\Administrator\View\Orders;
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Xdecaro\Component\Decarofinance\Administrator\Service\FinanceQueryService;
+
+final class HtmlView extends BaseHtmlView
+{
+    public array $items=[];
+    public array $accounts=[];
+    public array $budgetLines=[];
+    public bool $canApprove=false;
+    public bool $canExecute=false;
+    public bool $canEdit=false;
+
+    public function display($tpl=null): void
+    {
+        $app=Factory::getApplication(); $identity=$app->getIdentity();
+        if (!$identity->authorise('core.manage','com_decarofinance')) { throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'),403); }
+        ToolbarHelper::title(Text::_('COM_DECAROFINANCE_ORDERS'),'credit');
+        $wa=$app->getDocument()->getWebAssetManager(); $wa->getRegistry()->addExtensionRegistryFile('com_decarofinance'); $wa->useStyle('com_decarofinance.admin');
+        $query=Factory::getContainer()->get(FinanceQueryService::class);
+        $this->items=$query->listOrders(); $this->accounts=$query->listAccounts(); $this->budgetLines=$query->listBudgetLines();
+        $this->canApprove=$identity->authorise('finance.approve','com_decarofinance');
+        $this->canExecute=$identity->authorise('finance.execute','com_decarofinance');
+        $this->canEdit=$identity->authorise('core.edit','com_decarofinance');
+        parent::display($tpl);
+    }
+}
