bd7955fc4044bbab260eb520aec25eaf3c12247d
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-20 02:54:57 +0200

    Finance 1.6.0: simplify transfers and display local time

--- component/admin/tmpl/transfers/default.php
+++ component/admin/tmpl/transfers/default.php
@@ -4,6 +4,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
+use Xdecaro\Component\Decarofinance\Administrator\Helper\UiHelper;
 ?>
 <div class="xdf-finance">
   <section class="xdecaro-card xdf-form-card">
@@ -29,13 +30,8 @@ use Joomla\CMS\Router\Route;
         <label><?php echo Text::_('COM_DECAROFINANCE_AMOUNT'); ?><input class="form-control" name="amount" required inputmode="decimal"></label>
         <label><?php echo Text::_('COM_DECAROFINANCE_CURRENCY'); ?><input class="form-control" name="currency" value="EUR" maxlength="3" required></label>
         <label><?php echo Text::_('JDATE'); ?><input class="form-control" type="datetime-local" name="occurred_at"></label>
-        <label><?php echo Text::_('COM_DECAROFINANCE_EXTERNAL_KEY'); ?><input class="form-control" name="external_key" maxlength="191"></label>
         <label class="xdf-span-2"><?php echo Text::_('COM_DECAROFINANCE_DESCRIPTION'); ?><textarea class="form-control" name="description" rows="2" maxlength="500"></textarea></label>
-        <details class="xdf-span-2 xdf-advanced"><summary><?php echo Text::_('COM_DECAROFINANCE_SOURCE_REFERENCE'); ?></summary><div class="xdf-form-grid">
-          <label><?php echo Text::_('COM_DECAROFINANCE_SOURCE_COMPONENT'); ?><input class="form-control" name="source_component" placeholder="com_xxx"></label>
-          <label><?php echo Text::_('COM_DECAROFINANCE_SOURCE_ENTITY'); ?><input class="form-control" name="source_entity"></label>
-          <label><?php echo Text::_('COM_DECAROFINANCE_SOURCE_ID'); ?><input class="form-control" name="source_id"></label>
-        </div></details>
+        <div class="xdf-span-2"><div class="form-text"><?php echo Text::_('COM_DECAROFINANCE_EXTERNAL_KEY_AUTOMATIC_HELP'); ?></div></div>
         <div class="xdf-span-2"><button class="btn btn-primary" type="submit"><?php echo Text::_('COM_DECAROFINANCE_TRANSFER'); ?></button></div>
         <?php echo HTMLHelper::_('form.token'); ?>
       </form>
@@ -50,7 +46,7 @@ use Joomla\CMS\Router\Route;
         <?php foreach($this->items as $row): ?>
           <tr>
             <td><?php echo (int)$row['id']; ?></td>
-            <td><?php echo htmlspecialchars((string)$row['occurred_at'],ENT_QUOTES,'UTF-8'); ?></td>
+            <td><?php echo htmlspecialchars(UiHelper::formatDateTime((string)$row['occurred_at']),ENT_QUOTES,'UTF-8'); ?></td>
             <td><?php echo htmlspecialchars((string)($row['from_account_name']??''),ENT_QUOTES,'UTF-8'); ?></td>
             <td><?php echo htmlspecialchars((string)($row['to_account_name']??''),ENT_QUOTES,'UTF-8'); ?></td>
             <td><?php echo number_format((float)$row['amount'],2,',','.').' '.htmlspecialchars((string)$row['currency'],ENT_QUOTES,'UTF-8'); ?></td>
